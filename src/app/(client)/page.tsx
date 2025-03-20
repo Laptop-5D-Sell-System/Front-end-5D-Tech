3b1889b089799cf44c68ce130c9ff654fbdac7f8
DuongVQ <duongvq392@gmail.com>
2025-03-20 10:38:07 +0700

    20/03

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -214,41 +214,203 @@ export default function Home() {
             {/* End banner two */}
 
             {/* Start featured products */}
-            <div className="featured_products">
+            <div className="featured_products mt-[100px]">
                 <div className="header_featured_products">
                     <div className="">
                         <p>KHUYẾN MÃI LỚN</p>
                         <h2>Sản Phẩm Nổi Bật</h2>
                     </div>
-                    <div className="featured_products_list">
+                    <div className="featured_products_list grid grid-cols-4 gap-5">
                         <div className="featured_products_item">
                             <div className="featured_products_img">
-                                <div className="featured_products_sale">
-                                    -40%
-                                </div>
+                                <div className="featured_products_sale">-40%</div>
                                 <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
                             </div>
-                            <div className="featured_products_category">
-                                Máy tính
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
                             </div>
-                            <div className="featured_products_name">
-                                Dell XPS 13 9340
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
                             </div>
-                            <div className="featured_products_rate">
-                                x x x x x
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
                             </div>
-                            <div className="featured_products_quantity">
-                                Có sẵn: 8/40
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
                             </div>
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
+                            </div>
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
+                            </div>
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
+                            </div>
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
+                            </div>
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
+                            </div>
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
+                            </div>
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
+                            </div>
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
+                            </div>
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
+                            <div className="featured_products_price">
+                                <div className="old_price">31.000.000 đ</div>
+                                <div className="new_price">29.000.000 đ</div>
+                            </div>
+                        </div>
+                        <div className="featured_products_item">
+                            <div className="featured_products_img">
+                                <div className="featured_products_sale">-40%</div>
+                                <Image src="/images/laptop.jpeg" alt="laptop" width={200} height={100} />
+                            </div>
+                            <div className="featured_products_category">Máy tính</div>
+                            <div className="featured_products_name">Dell XPS 13 9340</div>
+                            <div className="featured_products_rate">x x x x x</div>
+                            <div className="featured_products_quantity">Có sẵn: 8/40</div>
                             <div className="featured_products_price">
                                 <div className="old_price">31.000.000 đ</div>
                                 <div className="new_price">29.000.000 đ</div>
                             </div>
                         </div>
                     </div>
+                    <div className="btn w-full text-center mt-5">
+                        <Button>Xem Thêm</Button>
+                    </div>
                 </div>
             </div>
             {/* End featured products */}
+
+            {/* Start banner three */}
+            <div className="banner_three group mt-[100px] h-[200px] relative overflow-hidden rounded bg-yellow-300 text-white flex items-center justify-center">
+                <Image
+                    src="/images/earphone-2-1-1.png"
+                    alt="tai nghe"
+                    width={300}
+                    height={100}
+                    className="absolute bottom-0 left-[100px] transition-transform duration-500 group-hover:scale-110"
+                ></Image>
+                <p className="font-semibold text-3xl mx-[100px]">
+                    Super Friendly <br /> Electronics Store
+                </p>
+                <Button>Mua Ngay</Button>
+                <Image
+                    src="/images/watch-1.png"
+                    alt="dong ho"
+                    width={150}
+                    height={150}
+                    className="absolute top-3 right-[100px] transition-transform duration-500 group-hover:scale-110"
+                ></Image>
+            </div>
+            {/* End banner three */}
+
+            {/* Start brands */}
+            <div className="brand_list grid grid-cols-4 gap-5 mt-[100px]">
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+                <div className="brand_item border">
+                    <Image src="/images/brand-3-1-1.svg" alt="nhãn hàng" width={150} height={100}></Image>
+                </div>
+            </div>
+            <div className="w-full h-[200px] mt-5 relative overflow-hidden">
+                <Image src="/images/fake1.png" alt="" fill className="absolute " quality={100}></Image>
+            </div>
+            {/* End brands */}
+
+            {/* Start contact */}
+            <div className="contact mt-[100px]">
+                <div className="contact_info grid grid-cols-3 gap-5">
+                    <div className="contact_info_item">
+                        <div className="icon w-[50px] h-[50px] mx-auto mb-2 rounded-full flex items-center justify-center">
+                            <Mail color="red" strokeWidth={1} />
+                        </div>
+                        <p className="text-center">Email</p>
+                        <p className="text-center">
+                            <a href="mailto:" className="text-red-500"></a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* End contact */}
         </div>
     );
 }
